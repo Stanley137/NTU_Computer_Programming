d38e47087d9fcf080fa0fd8fe7c6dc4b539dee78
Stanley137 <84568187+Stanley137@users.noreply.github.com>
2023-09-15 09:56:25 +0800

    upload files

--- bouncing_ball.cpp
+++ bouncing_ball.cpp
@@ -7,7 +7,7 @@ int check_wall(int x1, int y1, int X, int Y){
 void swap(int * a,int * b){
     int temp = *a;
     *a = *b;
-    *b = *a;
+    *b = temp;
 }
 int main(){
     int box[301][301] = {0};
@@ -30,8 +30,6 @@ int main(){
     }
     for(int t=0;t<times;t++){
         // ball_1 move
-        x1 += dx1;
-        y1 += dy2;
         if(check_wall(x1,y1,X,Y)){
             // which wall
             if((x1==1 && y1==1) || (x1==1 && y1==Y) ||
@@ -44,20 +42,48 @@ int main(){
             else if(y1==Y || y1==1) // up || down
                 dy1 = -dy1;
         }
-        else if((x1)==x2 && (y1)==y2){ // exchange position
+        if((x1)==x2 && (y1)==y2){ // exchange position
+            printf("Swap1\n");
             swap(&dx1,&dx2);
-            swap(&dy1,&dy1);
+            swap(&dy1,&dy2);
+            printf("%d %d\n",dx1,dy1);
+            printf("%d %d\n",dx2,dy2);
         }
+        x1 += dx1;
+        y1 += dy1;
+        printf("%d %d\n", x1,y1);
+        printf("%d %d\n",dx1,dy1);
         // ball_2 move
-        
+        if(check_wall(x2,y2,X,Y)){
+            // which wall
+            if((x2==1 && y2==1) || (x2==1 && y2==Y) ||
+               (x2==X && y2==1) || (x2==X && y2==Y)){
+                dx2 = -dx2;
+                dy2 = -dy2;          
+            }
+            else if(x2==X || x2==1)
+                dx2 = -dx2;
+            else if(y2==Y || y2==1)
+                dy2 = -dy2;
+        }
+        if(x1==x2 && y1==y2){
+            printf("Swap2\n");
+            swap(&dx1,&dx2);
+            swap(&dy1,&dy2);
+        }
         // visualize
         // for(int j=Y;j>=1;j--){
         //     for(int i=1;i<=X;i++)
+        x2 += dx2;
+        y2 += dy2;
+        printf("%d %d\n", x2,y2);
+        printf("%d %d\n",dx2,dy2);
         //         printf("%d ",box[i][j]);
         //     printf("\n");
         // }
         // printf("\n---------\n");
     }
+    printf("%d\n%d\n%d\n%d\n", x1,y1,x2,y2);
     // printf("%d %d\n", X,Y);
     // swap(&x2,&y2);
     // printf("%d %d\n", x2,y2);
