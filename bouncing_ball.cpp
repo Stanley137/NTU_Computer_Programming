f73b0dc179a7187044c6bd47e8ccc3412ef89c8c
Stanley137 <84568187+Stanley137@users.noreply.github.com>
2023-09-15 23:20:04 +0800

    upload files

--- bouncing_ball.cpp
+++ bouncing_ball.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int check_wall(int x1, int y1, int X, int Y){
+int out_wall(int x1, int y1, int X, int Y){
     if(x1==X || x1==1 || y1==Y || y1==1)
         return 1;
     return 0;
@@ -21,15 +21,14 @@ int main(){
     scanf("%d", &times);
     for(int t=0;t<times;t++){
         // ball_1 move
-        if(check_wall(x1,y1,X,Y) && x1==x2 && y1==y2){
-            if (x1 == X || x1 == 1) // right || left
-                dx1 = -dx1;
-            if (y1 == Y || y1 == 1) // up || down
-                dy1 = -dy1;
+        if((x1)==x2 && (y1)==y2){ // exchange position
+            // printf("Swap1\n");
             swap(&dx1,&dx2);
             swap(&dy1,&dy2);
+            // printf("%d %d\n",dx1,dy1);
+            // printf("%d %d\n",dx2,dy2);
         }
-        else if(check_wall(x1,y1,X,Y)){
+        if(out_wall(x1,y1,X,Y)){
             // which wall
             // if((x1==1 && y1==1) || (x1==1 && y1==Y) ||
             //    (x1==X && y1==1) || (x1==X && y1==Y)){
@@ -41,27 +40,18 @@ int main(){
             if(y1==Y || y1==1) // up || down
                 dy1 = -dy1;
         }
-        else if((x1)==x2 && (y1)==y2){ // exchange position
-            // printf("Swap1\n");
-            swap(&dx1,&dx2);
-            swap(&dy1,&dy2);
-            // printf("%d %d\n",dx1,dy1);
-            // printf("%d %d\n",dx2,dy2);
-        }
         x1 += dx1;
         y1 += dy1;
-        printf("%d %d\n", x1,y1);
+        // printf("%d %d\n", x1,y1);
         // printf("%d %d\n",dx1,dy1);
         // ball_2 move
-        if(check_wall(x2,y2,X,Y) && x1==x2 && y1==y2){
-            if(x2==X || x2==1)
-                dx2 = -dx2;
-            if(y2==Y || y2==1)
-                dy2 = -dy2;
-            swap(&dx1,&dx2);
-            swap(&dy1,&dy2);
-        }
-        else if(check_wall(x2,y2,X,Y)){
+        // don't check, because the present round hasn't complete
+        // if(x1==x2 && y1==y2){
+        //     // printf("Swap2\n");
+        //     swap(&dx1,&dx2);
+        //     swap(&dy1,&dy2);
+        // }
+        if(out_wall(x2,y2,X,Y)){
             // which wall
             // if((x2==1 && y2==1) || (x2==1 && y2==Y) ||
             //    (x2==X && y2==1) || (x2==X && y2==Y)){
@@ -73,14 +63,9 @@ int main(){
             if(y2==Y || y2==1)
                 dy2 = -dy2;
         }
-        else if(x1==x2 && y1==y2){
-            // printf("Swap2\n");
-            swap(&dx1,&dx2);
-            swap(&dy1,&dy2);
-        }
         x2 += dx2;
         y2 += dy2;
-        printf("%d %d\n", x2,y2);
+        // printf("%d %d\n", x2,y2);
         // printf("%d %d\n",dx2,dy2);
     }
     printf("%d\n%d\n%d\n%d\n", x1,y1,x2,y2);
