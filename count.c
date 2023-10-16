32d3e3d88e2c29c092bcb8fe7805652692819bf3
Stanley137 <84568187+Stanley137@users.noreply.github.com>
2023-10-16 14:15:22 +0800

    Debugging

---
+++ count.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define N 512
+void count(int **p[]){
+    int * second_order[N];
+    int final_num_count[N] = {0};
+    int count_index = 0;
+    for(int i=0;p[i] != NULL; i++){
+        // check second_order_list
+        for(int j=0;j<count_index;j++){
+            if(*p[i] == second_order[j]){
+                final_num_count[j]++;
+                break;
+            }
+        }
+        // if not, insert to second_order_list
+        second_order[count_index] = *p[i];
+        final_num_count[count_index] = 1;
+        count_index++;
+    }
+    // sort reach times
+    for(int i=count_index-2;i>0;i--){
+        for(int j=0;j<i;j++){
+            if(final_num_count[j] > final_num_count[j+1]){
+                int temp = final_num_count[j];
+                final_num_count[j] = final_num_count[j+1];
+                final_num_count[j+1] = temp;
+                int * temp_ptr = second_order[j];
+                second_order[j] = second_order[j+1];
+                second_order[j+1] = temp_ptr;
+            }
+        }
+    }
+    // sort value
+    for(int i=0;i<=count_index-2;i++){
+        for(int j=i;j<=count_index-2;j++){
+            if(*second_order[j] > *second_order[j+1] && (final_num_count[j]==final_num_count[j+1])){
+                int *temp_ptr = second_order[j];
+                second_order[j] = second_order[j+1];
+                second_order[j+1] = temp_ptr;
+            }
+        }
+    }
+    // print out
+    for(int i=0;i<count_index;i++){
+        printf("%d %d\n", *second_order[i], final_num_count[i]);
+    }
+
+}
