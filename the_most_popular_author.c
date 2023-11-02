3288207016e960b41916a5f9a3485c10e569b894
Stanley137 <84568187+Stanley137@users.noreply.github.com>
2023-11-02 15:15:05 +0800

    upload files

---
+++ the_most_popular_author.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int m;
+    scanf("%d", &m);
+    char *book[100], *author[100], *final_author[100];
+    for(int i = 0;i<m;i++){
+        scanf("%s%s",author[i], book[i]);
+    }
+}
