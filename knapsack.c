96d6022517b6224f2c35d9d44770aca4f014ab9a
Stanley137 <84568187+Stanley137@users.noreply.github.com>
2023-11-14 20:33:47 +0800

    upload files

--- knapsack.c
+++ knapsack.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <limits.h>
 struct object
 {
     int w;
     int v;
 };
 typedef struct object Obj;
-int knapsack(Obj obj[], int W, int n,int value,int weight){
-
+int max(int a,int b){
+    if(a>b)
+        return a;
+    else 
+        return b;
 }
-
+int knapsack(Obj obj[], int W, int n,int k){
+    if(k>=n)
+        return 0;
+    if(W>=obj[k].w)
+        return max(knapsack(obj,W-obj[k].w,n,k+1)+obj[k].v,knapsack(obj,W,n,k+1));
+    return knapsack(obj,W,n,k+1); // find the next object
+}
 int main(){
     int n, W;
     Obj obj[20];
     scanf("%d%d", &n,&W);
     for(int i=0;i<n;i++)
         scanf("%d%d", &obj[i].w,&obj[i].v);
-    
+    printf("%d\n", knapsack(obj,W,n,0));
+    return 0;
 }
