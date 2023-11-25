9d248b037feb5b4760abbe568b2ad916cb19853c
Stanley137 <stanley2friend@gmail.com>
2023-11-25 10:05:18 +0800

    dealing sample test2

--- 50098_disjoint_club.c
+++ 50098_disjoint_club.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
-int find_disjoint(uint64_t clubs,int i,int N,int K){
-    
+void print_bit(bool chooses[],int N){
+    for(int i=0;i<N;i++){
+        if(chooses[i])
+            printf("%d\n", i);
+    }
+}
+bool find_disjoint(uint64_t clubs[],uint64_t choose_club,bool chooses[],int k,int N,int K){
+    if(k==K){
+        uint64_t choose = choose_club;
+        print_bit(chooses, N);
+        for(int i=0;i<N;i++){
+            if(chooses[i]){
+                choose -= clubs[i];
+            }
+        }
+        if(choose == 0)
+            return 1;
+        else 
+            return 0;
+    }
+    uint64_t choose;
+    for(int i=0;i<N;i++){
+        if(chooses[i])
+            continue;
+        chooses[i] = 1;
+        choose = choose_club;
+        choose |= clubs[i];
+        //printf("i: %d\nchoose:\n", i);
+        //print_bit(chooses, N);
+        if(find_disjoint(clubs,choose,chooses,k+1,N,K))
+            return 1;
+        chooses[i] = 0;
+        choose_club = choose;
+    }
 }
 
+
 int main(){
-    int N,K,id;
+    int M,N,K,id;
     uint64_t clubs[105] = {0};
+    bool chooses[105] = {0};
     scanf("%d%d",&N,&K);
     for(int i=0;i<N;i++){
-        while(scanf("%d", &id) != EOF){
-            clubs[i] += (1<<id);
+        scanf("%d", &M);
+        for(int j=0;j<M;j++){
+            scanf("%d", &id);
+            clubs[i] |= (1<<id);
         }
+        //printf("%llu\n", clubs[i]);
     }
-    
+    find_disjoint(clubs,0,chooses,0,N,K);
+    print_bit(chooses, N);
+    return 0;
 }
